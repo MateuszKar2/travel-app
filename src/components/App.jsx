d8a8c76c24545c1f7515522e36ccae0c650b0f85
MateuszKar2 <system505@protonmail.com>
2024-02-23 12:16:24 +0100

    add file test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+
+import { MyPagesTest } from "./MyPagesTest/MyPagesTest";
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+     <div>
       React homework template
+      <MyPagesTest/>
     </div>
   );
 };
