df4316fc1019349c34290f6f2861d88924c986f4
MateuszKar2 <system505@protonmail.com>
2024-03-01 19:07:09 +0100

    add file

---
+++ src/components/pages/Europe.jsx
@@ -0,0 +1,16 @@
+
+
+export const Europe = () => {
+
+    const imageUrl = "file:///C:/Users/48698/Downloads/valencia-1049389_640.jpg"
+
+
+    return(
+        <>
+            Hiszpania
+        <p>
+            <img src={imageUrl} alt="Valencia" width="640" />
+        </p>
+        </>
+    )
+}
