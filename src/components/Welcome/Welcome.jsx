2644faf985738c9072a22f9ed04b06f172eb9442
MateuszKar2 <system505@protonmail.com>
2024-03-03 22:02:26 +0100

    add content

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,10 +1,18 @@
 
-
+import styles from './Welcome.module.css'
 
 export const Welcome = () => {
     return(
         <>
-            <div>Jestem</div>
+        <div className={styles.welcome__picture}>
+            <div className={styles.heading}>
+                <button className={styles.btn} type="button"><p className={styles.btn__text}>Welcome</p></button>
+                <button className={styles.btn} type="button"><p className={styles.btn__text}>Countries</p></button>
+                <button className={styles.btn} type="button"><p className={styles.btn__text}>Prices</p></button>
+                <button className={styles.btn} type="button"><p className={styles.btn__text}>Sign up</p></button>
+            </div>
+        </div>
+            
         </>
     )
 }
