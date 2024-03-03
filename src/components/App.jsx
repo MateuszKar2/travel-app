5f36050b45abbabf66a99053a46a9fc86f8ca564
MateuszKar2 <system505@protonmail.com>
2024-03-03 22:01:59 +0100

    change styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 
-import { Heading } from "./Headiing/Heading";
+
 import { Welcome } from "./Welcome/Welcome";
 
 export const App = () => {
   return (
      <>
-     <Heading></Heading>
-     <Welcome></Welcome>
+     
+     <Welcome>
+
+     </Welcome>
 
      </>
   );
