3b3fcce8f51fe0717132c4a679341cc556ce878f
MateuszKar2 <system505@protonmail.com>
2024-05-08 21:31:24 +0200

    add file

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,10 +1,15 @@
 
-import styles from './Welcome.module.css'
+import styles from './Welcome.module.css';
+
+const imageURL = ''
+
+
 
 export const Welcome = () => {
     return(
         <>
-        <div className={styles.welcome__picture}>
+        <div>
+            <img src={imageURL} alt="River" width="640" />
             <div className={styles.heading}>
                 <button className={styles.btn} type="button"><p className={styles.btn__text}>Welcome</p></button>
                 <button className={styles.btn} type="button"><p className={styles.btn__text}>Countries</p></button>
